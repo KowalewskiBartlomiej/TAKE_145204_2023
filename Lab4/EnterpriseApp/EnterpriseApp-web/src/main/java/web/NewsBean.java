9b5228f347eebca8f5cd1c0b58033b4b1d0f363c
Bartłomiej Kowalewski <56280953+KowalewskiBartlomiej@users.noreply.github.com>
2023-04-01 11:34:09 +0200

    Add files via upload

---
+++ Lab4/EnterpriseApp/EnterpriseApp-web/src/main/java/web/NewsBean.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package web;
+
+import ejb.NewsItem;
+import ejb.NewsItemFacadeLocal;
+import jakarta.annotation.Resource;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.jms.JMSContext;
+import jakarta.jms.JMSException;
+import jakarta.jms.ObjectMessage;
+import jakarta.jms.TextMessage;
+import java.util.List;
+
+/**
+ *
+ * @author kowal
+ */
+@Named(value = "newsBean")
+@RequestScoped
+public class NewsBean {
+    
+    
+
+    @Inject
+    private NewsItemFacadeLocal facade;
+
+    @Inject
+    private JMSContext context;
+    
+    @Resource(lookup = "jms/NewsQueue")
+    private jakarta.jms.Queue queue;
+
+    private String headingText;
+    private String bodyText;
+    
+    /**
+     * Creates a new instance of NewsBean
+     */
+    public NewsBean() {
+    }
+
+    void sendNewsItem(String heading, String body) {
+        try {
+            TextMessage message = context.createTextMessage();
+            message.setText(String.join("|", heading, body));
+            context.createProducer().send(queue, message);
+        } catch (JMSException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String submitNews() {
+        sendNewsItem(getHeadingText(), getBodyText());
+        return null;
+    }
+
+    public List<NewsItem> getNewsItems() {
+        return facade.getAllNewsItems();
+    }
+
+    /**
+     * @return the headingText
+     */
+    public String getHeadingText() {
+        return headingText;
+    }
+
+    /**
+     * @param headingText the headingText to set
+     */
+    public void setHeadingText(String headingText) {
+        this.headingText = headingText;
+    }
+
+    /**
+     * @return the bodyText
+     */
+    public String getBodyText() {
+        return bodyText;
+    }
+
+    /**
+     * @param bodyText the bodyText to set
+     */
+    public void setBodyText(String bodyText) {
+        this.bodyText = bodyText;
+    }
+
+}
